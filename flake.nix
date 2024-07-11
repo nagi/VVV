8b83d31f649deeffdd79f117ba628cd6287d33ff
Andrew Nagi <andrew.nagi@gmail.com>
2024-07-11 23:52:09 +0100

    Simplify

--- flake.nix
+++ flake.nix
@@ -9,10 +9,24 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        localOverlay = import ./nix/overlay.nix;
         pkgs = import nixpkgs {
           system = "${system}";
-          overlays = [ localOverlay ];
         };
-      in { inherit (pkgs) devShells; });
+      in {
+        devShells.default = pkgs.mkShell rec {
+            name = "vagrant";
+
+            packages = [
+              pkgs.ruby_3_3
+            ];
+
+            shellHook = ''
+              echo "Hello there! (don't forget to run dnsmasq)"
+
+              # Prepend binstubs to PATH for development, which causes Vagrant-agogo
+              # to use the legacy Vagrant in this repo. See client.initVagrantRubyRuntime
+              PATH=$PWD/binstubs:$PATH
+            '';
+        };
+      });
 }
